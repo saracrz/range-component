5449978d887e1cfcae57735d34370658ea3409b0
saracrz <sara.crz@gmail.com>
2023-01-20 18:54:18 +0000

    Add Range component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Range } from "./Range";
+import ReactDOM from 'react-dom/client';
+
+// ReactDOM.render(<Range name="Taylor" />);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Range />);
